0177afbdeec59c80101483e2d3d56afaa65b993f
Anmol Srivastava <a4anmol93.6@gmail.com>
2025-05-01 17:56:10 +0530

    hm

--- src/app/api/snippets/[id]/route.ts
+++ src/app/api/snippets/[id]/route.ts
@@ -97,7 +97,7 @@ export async function GET(
 
     return NextResponse.json({
       ...snippet,
-      isLiked: userId ? snippet.likes.length > 0 : false,
+      isLiked: userId ? (snippet.likes?.length ?? 0) > 0 : false,
       likes: undefined, // Remove the likes array from the response
     });
   } catch (error) {
